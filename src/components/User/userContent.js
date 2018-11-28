672ce7f12477df7b8b3a4ad07949d9e4e6e51a43
vstymkovskyi <vstymkovskyi@exadel.com>
2018-11-28 15:23:49 +0200

    user overview tab

--- src/components/User/userContent.js
+++ src/components/User/userContent.js
@@ -111,6 +111,20 @@ class UserTabs extends Component {
                   <strong>Company: </strong><span>{userData.company}</span>
                 </div>
               }
+              { userData.location != null &&
+                <div className="p-note user-profile-location">
+                  <svg className="octicon octicon-location m-2"
+                       viewBox="0 0 12 16"
+                       version="1.1"
+                       width="12"
+                       height="16"
+                       aria-hidden="true">
+                    <path fill-rule="evenodd"
+                          d="M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path>
+                  </svg>
+                  {userData.location}
+                </div>
+              }
               <a href={userData.html_url} target={"blank"} className={"btn btn-secondary btn-sm"}>view git profile</a>
             </Col>
           </Row>

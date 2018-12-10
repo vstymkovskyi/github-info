88f1d4d84250279653ca82ef25d3cebde6ce9565
vstymkovskyi <vstymkovskyi@exadel.com>
2018-12-10 15:55:44 +0200

    change Navlink to Link

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,7 +7,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { Container, Row, Col } from 'reactstrap';
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class HomePage extends Component {
 
@@ -16,8 +16,8 @@ class HomePage extends Component {
       <React.Fragment>
         <p>At first please sign in to your account or create a new account locally.</p>
         <p className="lead">
-          <NavLink className="btn btn-primary btn-sm mr-2" to="/login">Sign in</NavLink>
-          <NavLink className="btn btn-primary btn-sm" to="/register">Register</NavLink>
+          <Link className="btn btn-primary btn-sm mr-2" to="/login">Sign in</Link>
+          <Link className="btn btn-primary btn-sm" to="/register">Register</Link>
         </p>
       </React.Fragment>
     );
@@ -26,8 +26,8 @@ class HomePage extends Component {
   pageContentAuthorized = () => {
     return (
       <React.Fragment>
-        <p>Now you can make some <NavLink className="btn btn-info btn-sm" to="/search">Search</NavLink> or go to
-           your <NavLink className="btn btn-info btn-sm" to="/user">profile page</NavLink> and check your account
+        <p>Now you can make some <Link className="btn btn-info btn-sm" to="/search">Search</Link> or go to
+           your <Link className="btn btn-info btn-sm" to="/user">profile page</Link> and check your account
            details.</p>
       </React.Fragment>
     );
@@ -43,8 +43,7 @@ class HomePage extends Component {
               <h1 className="display-4">Hello</h1>
               <p className="lead">It's a pet project to show my knowledge in React, Redux, Github API, Firebase and etc.</p>
               <hr className="my-4" />
-              {!loggedIn && this.pageContentAnonymous()}
-              {loggedIn && this.pageContentAuthorized()}
+              {loggedIn ? this.pageContentAuthorized() : this.pageContentAnonymous()}
             </div>
           </Col>
         </Row>
